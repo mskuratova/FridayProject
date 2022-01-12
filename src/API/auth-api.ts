af88b37e3ea23f1a0cf11d4b5d2177a0c35d648d
Borislav <borislavizmestiev@gmail.com>
2022-01-12 16:09:51 +0300

    Create Reset Password Page

--- src/API/auth-api.ts
+++ src/API/auth-api.ts
@@ -1,10 +1,18 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://neko-back.herokuapp.com/2.0',
+    baseURL: 'https://neko-back.herokuapp.com/2.0/',
     // baseURL: 'http://localhost:7542/2.0/',
 });
 
-const authAPI = {
+type ResetPasswordValuesType = {
+    email: string
+    from: string
+    message: string
+};
 
+export const authAPI = {
+    resetPassword: (resetPasswordValues: ResetPasswordValuesType) => {
+        return instance.post(`auth/forgot`, resetPasswordValues)
+    }
 }
--- src/Components/ResetPassword/ResetPassword.tsx
+++ src/Components/ResetPassword/ResetPassword.tsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import {useFormik} from "formik";
+import {authAPI} from "../../API/auth-api";
 
-export const ResetPassword = ()=>{
-    return(
+export const ResetPassword = () => {
+
+    const [statusText, setStatusText] = useState<string>('');
+    const [error, setError] = useState<boolean>(false);
+
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            from: 'From test Friday',
+            // message: 'Test Text',
+            message: "\n<div style=\"background-color: lime; padding: 15px\">\npassword recovery link: \n<a href='https://uladzislaupapliouka.github.io/newPassword/$token$'>link</a>\n</div>\n",
+        },
+
+        validate: (values) => {
+            const errors: Partial<{ email: string }> = {};
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (values.email.length < 3) {
+                errors.email = 'Invalid email address';
+            }
+            return errors
+        },
+
+        onSubmit: values => {
+            authAPI.resetPassword(values)
+                .then(res => {
+                    if (res.status === 200) {
+                        console.log(res);
+                        setStatusText(res.data.info)
+                    }
+                }).catch(err => {
+                setError(true);
+                setStatusText('Email Not Found');
+
+            })
+            formik.resetForm();
+        }
+    })
+
+    return (
         <div>
             <h1>ResetPassword</h1>
+            <form onClick={formik.handleSubmit}>
+                {statusText}
+                <input
+                    placeholder={'Email'}
+                    {...formik.getFieldProps('email')}
+                />
+                <button disabled={!formik.isValid || !formik.dirty} type={'submit'}>Send Instructions</button>
+            </form>
         </div>
     )
 }
